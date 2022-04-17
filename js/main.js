a5302f6b3cb69c11428863ce52cc79ad04905b96
margaretau <aumarg.edu@gmail.com>
2022-04-16 19:28:51 -0700

    converted the fetch GOT houses json code to async-await functions. This is the code for the Bonus Challenge

--- js/main.js
+++ js/main.js
@@ -1,10 +1,4 @@
-//This code does NOT create any global variables.
-//Promises can be chained together, with the previous promise
-// passing its results to the next one in the chain.
-// the format is: fetch().then().then().catch()
-//it's easier to read if we put each step in its own line,
-//that's why the periods start the then lines.
-
+/* Retain previous promise code sections for comparison
 fetch("houses.json")
     .then((response) => response.json())
     .then((data) => {
@@ -32,12 +26,8 @@ fetch("houses.json")
         container.innerHTML = html;
     })
     .catch((err) => console.log("There was an error:", err));
-//this only runs if there is an error during the above process
+    //this only runs if there is an error during the above process
 
-/* part 2 of assignment - make external json call for random dark color then
-set it as the background color for the page. Make text color white for the dark background */
-
-/*
 fetch('https://x-colors.herokuapp.com/api/random/hue?type=dark')
     .then((response) => response.json())
     .then((data) => {
@@ -47,12 +37,46 @@ fetch('https://x-colors.herokuapp.com/api/random/hue?type=dark')
     })
     .catch((err) => console.log("There was an error:", err));
 */
-    //extra credit - convert the gathering of the code in a function call then create an async function that compiles the html string. then call the function
+
+/*extra credit - convert the gathering of the json in a function call then create an async function that compiles the html string. then call the function; this time fetch a light background with dark text as variation. */
+
+//Function to gather the house json information
+async function getHouses() {
+    let response = await fetch("houses.json");
+    console.log('house response is', response);
+    return response.json();
+}
+
+// function to compile the html string to display the json information
+async function processHouses() {
+    let houses = await getHouses();
+    console.log(houses);
+    let html = `<p class="list">Game of Thrones List</p><dl class="GoT-House-List">`;
+    houses.forEach((house) => {
+        let objInfo = `<dt class="house">${house.name}</dt>`;
+        house.members.forEach((member) => {
+            objInfo += `<dd class="folks">${member}</dd>`;
+        });
+
+        // generate the html snippet for one array item and add to the "html" temp holder.
+        html += objInfo;
+    });
+    //close the html with the closing tag
+    html += `</dl>`;
+
+    //make a reference to the html container where
+    //the info will be displayed.
+    const container = document.querySelector("#container");
+    container.innerHTML = html;
+}
+
+//Function to gather the random color json information
 async function getColors() {
     let response = await fetch('https://x-colors.herokuapp.com/api/random/hue?type=light');
     console.log('response is', response);
     return response.json();
 }
+// function to use that random color for the page background
 async function processColors() {
     console.log('here');
     let colors = await getColors();
@@ -62,4 +86,6 @@ async function processColors() {
     mainBody.style.color = "black";
 }
 
+// call those functions
+processHouses();
 processColors();
