990d7b5f6dfc1833c5607bb5a8573c03efcf1e1c
margaretau <aumarg.edu@gmail.com>
2022-04-17 23:34:17 -0700

    updated async code to include catch error

--- js/main.js
+++ js/main.js
@@ -42,48 +42,62 @@ fetch('https://x-colors.herokuapp.com/api/random/hue?type=dark')
 
 //Function to gather the house json information
 async function getHouses() {
-    let response = await fetch("houses.json");
-    console.log('house response is', response);
-    return response.json();
+    try {
+        let response = await fetch("houses.json");
+        let data = await response.json();
+        return data;
+    } catch (err) {
+        console.log('There is an error', err);
+    }
 }
 
 // function to compile the html string to display the json information
 async function processHouses() {
-    let houses = await getHouses();
-    console.log(houses);
-    let html = `<p class="list">Game of Thrones List</p><dl class="GoT-House-List">`;
-    houses.forEach((house) => {
-        let objInfo = `<dt class="house">${house.name}</dt>`;
-        house.members.forEach((member) => {
-            objInfo += `<dd class="folks">${member}</dd>`;
+    try {
+        let houses = await getHouses();
+        let html = `<p class="list">Game of Thrones List</p><dl class="GoT-House-List">`;
+
+        houses.forEach((house) => {
+            let objInfo = `<dt class="house">${house.name}</dt>`;
+            house.members.forEach((member) => {
+                objInfo += `<dd class="folks">${member}</dd>`;
+            });
+
+            // generate the html snippet for one array item and add to the "html" temp holder.
+            html += objInfo;
         });
+        //close the html with the closing tag
+        html += `</dl>`;
 
-        // generate the html snippet for one array item and add to the "html" temp holder.
-        html += objInfo;
-    });
-    //close the html with the closing tag
-    html += `</dl>`;
-
-    //make a reference to the html container where
-    //the info will be displayed.
-    const container = document.querySelector("#container");
-    container.innerHTML = html;
+        //make a reference to the html container where
+        //the info will be displayed.
+        const container = document.querySelector("#container");
+        container.innerHTML = html;
+    } catch (err) {
+        console.log('There is an error', err);
+    }
 }
 
 //Function to gather the random color json information
 async function getColors() {
-    let response = await fetch('https://x-colors.herokuapp.com/api/random/hue?type=light');
-    console.log('response is', response);
-    return response.json();
+    try {
+        let response = await fetch('https://x-colors.herokuapp.com/api/random/hue?type=light');
+        let data = await response.json();
+        return data;
+    } catch (err) {
+        console.log('There is an error', err);
+    }
 }
 // function to use that random color for the page background
 async function processColors() {
-    console.log('here');
-    let colors = await getColors();
-    console.log('colors', colors);
-    let mainBody = document.getElementById("mainbody");
-    mainBody.style.backgroundColor = colors.hex;
-    mainBody.style.color = "black";
+    try {
+        let colors = await getColors();
+        let mainBody = document.getElementById("mainbody");
+        mainBody.style.backgroundColor = colors.hex;
+        mainBody.style.color = "black";
+    } catch (err) {
+        console.log('There is an error', err);
+    }
 }
 
 // call those functions
