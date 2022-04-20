c56894cb1065463e8e5e7ccbd770122b5bee993c
margaretau <aumarg.edu@gmail.com>
2022-04-19 18:02:34 -0700

    added more semantic tags and styles, changed refresh button to a user selected light or dark button

--- js/main.js
+++ js/main.js
@@ -55,10 +55,10 @@ async function getHouses() {
 async function processHouses() {
     try {
         let houses = await getHouses();
-        let html = `<p class="list">Game of Thrones List</p><dl class="GoT-House-List">`;
+        let html = `<h2 class="list">Game of Thrones List</h2><dl class="GoT-House-List">`;
 
         houses.forEach((house) => {
-            let objInfo = `<dt class="house">${house.name}</dt>`;
+            let objInfo = `<dt class="house"><h3>${house.name}</h3></dt>`;
             house.members.forEach((member) => {
                 objInfo += `<dd class="folks">${member}</dd>`;
             });
@@ -79,9 +79,9 @@ async function processHouses() {
 }
 
 //Function to gather the random color json information
-async function getColors() {
+async function getColors(shade) {
     try {
-        let response = await fetch('https://x-colors.herokuapp.com/api/random/hue?type=light');
+        let response = await fetch(`https://x-colors.herokuapp.com/api/random/hue?type=${shade}`);
         let data = await response.json();
         return data;
     } catch (err) {
@@ -89,17 +89,33 @@ async function getColors() {
     }
 }
 // function to use that random color for the page background
-async function processColors() {
+async function processColors(block, shade) {
     try {
-        let colors = await getColors();
-        let mainBody = document.getElementById("mainbody");
-        mainBody.style.backgroundColor = colors.hex;
-        mainBody.style.color = "black";
+        let colors = await getColors(shade);
+        block.style.backgroundColor = colors.hex;
+        if (shade === 'light') {
+            block.style.color = "black";
+        } else {
+            block.style.color = "white";
+        }
     } catch (err) {
         console.log('There is an error', err);
     }
 }
 
-// call those functions
+let mainBody = document.getElementById("mainbody");
+
+// call those functions to initialize the page
 processHouses();
-processColors();
+processColors(mainBody, "light");
+
+//change the color theme based on user selection
+let lighten = document.querySelector(".lighten");
+let darken = document.querySelector(".darken");
+
+lighten.addEventListener('click', (e) => {
+    processColors(mainBody, "light");
+});
+darken.addEventListener('click', (e) => {
+    processColors(mainBody, "dark");
+});
